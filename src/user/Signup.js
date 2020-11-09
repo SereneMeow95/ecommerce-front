ce6ac8d929b68ecd8f48bbe29dbfda5942d79afd
SereneMeow95 <66867156+SereneMeow95@users.noreply.github.com>
2020-11-09 21:05:12 +0800

    63. Environment variables

--- src/user/Signup.js
+++ src/user/Signup.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Layout from "../core/Layout";
+import {API} from "../config";
 
 //signup page
 const Signup = () => (
     <Layout title = "Signup Page" description = "Autism Care Ecommerce Web App Signup Page">
-        ...
+        {API}
     </Layout>
 );
 
