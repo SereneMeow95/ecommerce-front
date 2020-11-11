ccb0da10d2d18d4c3a530884b4b6b0493fa5fd10
SereneMeow95 <66867156+SereneMeow95@users.noreply.github.com>
2020-11-11 13:26:12 +0800

    84. Show products in card

--- src/core/Home.js
+++ src/core/Home.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import Layout from "./Layout";
 import {getProducts} from "./apiCore";
+import Card from "./Card";
 
 //home page
 const Home = () => {
@@ -34,10 +35,24 @@ const Home = () => {
     }, []);
 
     return (
-        <Layout title = "Home Page" description = "Autism Care Ecommerce Web App">
-            {JSON.stringify(productsByArrival)}
-            <hr />
-            {JSON.stringify(productsBySell)}
+        <Layout
+            title = "Home Page"
+            description = "Autism Care Ecommerce Web App"
+            className="container-fluid"
+        >
+            <h2 className="mb-4">New Arrivals</h2>
+            <div className="row">
+                {productsByArrival.map((product, i) => (
+                    <Card key={i} product={product} />
+                ))}
+            </div>
+
+            <h2 className="mb-4">Best Sellers</h2>
+            <div className="row">
+                {productsBySell.map((product, i) => (
+                    <Card key={i} product={product} />
+                ))}
+            </div>
         </Layout>
     );
 };
