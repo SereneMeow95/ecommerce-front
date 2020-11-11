2312020b92b4c0fcbe072ed06b6773cf83daeedc
SereneMeow95 <66867156+SereneMeow95@users.noreply.github.com>
2020-11-11 16:09:50 +0800

    96. Filter with price range

--- src/core/RadioBox.js
+++ src/core/RadioBox.js
@@ -1,10 +1,11 @@
-import React, {useState, useEffect, Fragment} from "react";
+import React, {useState, useEffect} from "react";
 
-const RadioBox = ({ prices}) => {
+const RadioBox = ({ prices, handleFilters }) => {
     const [value, setValue] = useState(0);
 
-    const handleChange = () => {
-
+    const handleChange = (event) => {
+        handleFilters(event.target.value);
+        setValue(event.target.value);
     };
 
     return prices.map((p, i) => (
@@ -12,8 +13,9 @@ const RadioBox = ({ prices}) => {
             <input
                 onChange={handleChange}
                 value={`${p._id}`}
+                name={p}
                 type="radio"
-                className="mr-2 ml-4"
+                className="mr-2"
             />
             <label className="form-check-label">{p.name}</label>
         </div>
--- src/core/Shop.js
+++ src/core/Shop.js
@@ -34,8 +34,27 @@ const Shop = () => {
         //console.log("SHOP", filters, filterBy);
         const newFilters = {...myFilters};
         newFilters.filters[filterBy] = filters;
+
+        if (filterBy === "price") {
+            let priceValues = handlePrice(filters);
+            newFilters.filters[filterBy] = priceValues;
+        }
+
         setMyFilters(newFilters);
     };
+
+    //to get the price range array by price id
+    const handlePrice = value => {
+        const data = prices;
+        let array = [];
+
+        for (let key in data) {
+            if (data[key]._id === parseInt(value)) {
+                array = data[key].array;
+            }
+        }
+        return array;
+    }
 
     return (
         <Layout
