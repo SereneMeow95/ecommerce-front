6bfade9d97c3a728d20e0a99cae807590afd9f4d
SereneMeow95 <66867156+SereneMeow95@users.noreply.github.com>
2020-11-11 15:23:34 +0800

    94. Fixed price range

---
+++ src/core/fixedPrices.js
@@ -0,0 +1,33 @@
+export const prices = [
+    {
+        _id: 0,
+        name: "Any",
+        array: []
+    },
+    {
+        _id: 1,
+        name: "RM0 to RM9",
+        array: [0, 9]
+    },
+    {
+        _id: 2,
+        name: "RM10 to RM19",
+        array: [10, 19]
+    },
+    {
+        _id: 3,
+        name: "RM20 to RM29",
+        array: [20, 29]
+    },
+    {
+        _id: 4,
+        name: "RM30 to RM39",
+        array: [30, 39]
+    },
+    {
+        _id: 5,
+        name: "More than RM40",
+        array: [40, 99]
+    }
+
+]
