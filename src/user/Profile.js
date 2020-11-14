445274a683323d412070e9d55ee504a5924e49a5
SereneMeow95 <66867156+SereneMeow95@users.noreply.github.com>
2020-11-14 17:05:43 +0800

    142. Get user info for profile update

--- src/user/Profile.js
+++ src/user/Profile.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Layout from '../core/Layout';
 import { isAuthenticated } from '../auth';
 import { Link} from 'react-router-dom';
-//import { read, update, updateUser } from './apiUser';
+import { read, update, updateUser } from './apiUser';
 
 const Profile = ({ match }) => {
     const [values, setValues] = useState({
@@ -14,10 +14,16 @@ const Profile = ({ match }) => {
     });
 
     const { token } = isAuthenticated();
-    const { name, email, password, error, success } = values;
+    const {
+        name,
+        email,
+        password,
+        error,
+        success
+    } = values;
 
     const init = userId => {
-        // console.log(userId);
+        //console.log(userId);
         read(userId, token).then(data => {
             if (data.error) {
                 setValues({ ...values, error: true });
@@ -31,61 +37,76 @@ const Profile = ({ match }) => {
         init(match.params.userId);
     }, []);
 
-    const handleChange = name => e => {
-        setValues({ ...values, error: false, [name]: e.target.value });
-    };
-
-    const clickSubmit = e => {
-        e.preventDefault();
-        update(match.params.userId, token, { name, email, password }).then(data => {
-            if (data.error) {
-                // console.log(data.error);
-                alert(data.error);
-            } else {
-                updateUser(data, () => {
-                    setValues({
-                        ...values,
-                        name: data.name,
-                        email: data.email,
-                        success: true
-                    });
-                });
-            }
-        });
-    };
-
-    const redirectUser = success => {
-        if (success) {
-            return <Redirect to="/cart" />;
-        }
-    };
-
-    const profileUpdate = (name, email, password) => (
-        <form>
-            <div className="form-group">
-                <label className="text-muted">Name</label>
-                <input type="text" onChange={handleChange('name')} className="form-control" value={name} />
-            </div>
-            <div className="form-group">
-                <label className="text-muted">Email</label>
-                <input type="email" onChange={handleChange('email')} className="form-control" value={email} />
-            </div>
-            <div className="form-group">
-                <label className="text-muted">Password</label>
-                <input type="password" onChange={handleChange('password')} className="form-control" value={password} />
-            </div>
-
-            <button onClick={clickSubmit} className="btn btn-primary">
-                Submit
-            </button>
-        </form>
-    );
+    // const handleChange = name => e => {
+    //     setValues({ ...values, error: false, [name]: e.target.value });
+    // };
+    //
+    // const clickSubmit = e => {
+    //     e.preventDefault();
+    //     update(match.params.userId, token, { name, password }).then(data => {
+    //         if (data.error) {
+    //             console.log(data.error);
+    //             alert(data.error);
+    //         } else {
+    //             updateUser(data, () => {
+    //                 setValues({
+    //                     ...values,
+    //                     name: data.name,
+    //                     //email: data.email,
+    //                     success: true
+    //                 });
+    //             });
+    //         }
+    //     });
+    // };
+    //
+    // const redirectUser = success => {
+    //     if (success) {
+    //         return <Redirect to="/cart" />;
+    //     }
+    // };
+    //
+    // const profileUpdate = (name, email, password) => (
+    //     <form>
+    //         <div className="form-group">
+    //             <label className="text-muted">Name</label>
+    //             <input
+    //                 type="text"
+    //                 onChange={handleChange('name')}
+    //                 className="form-control"
+    //                 value={name}
+    //             />
+    //         </div>
+    //         <div className="form-group">
+    //             <label className="text-muted">Email</label>
+    //             <input type="email" onChange={handleChange('email')} className="form-control" value={email} />
+    //         </div>
+    //         <div className="form-group">
+    //             <label className="text-muted">Password</label>
+    //             <input
+    //                 type="password"
+    //                 onChange={handleChange('password')}
+    //                 className="form-control"
+    //                 value={password}
+    //             />
+    //         </div>
+    //
+    //         <button onClick={clickSubmit} className="btn btn-primary">
+    //             Submit
+    //         </button>
+    //     </form>
+    // );
 
     return (
-        <Layout title="Profile" description="Update your profile" className="container-fluid">
+        <Layout
+            title="Profile"
+            description="Update your profile"
+            className="container-fluid"
+        >
             <h2 className="mb-4">Profile update</h2>
-            {profileUpdate(name, email, password)}
-            {redirectUser(success)}
+            {JSON.stringify(values)}
+            {/*{profileUpdate(name, email, password)}*/}
+            {/*{redirectUser(success)}*/}
         </Layout>
     );
 };
